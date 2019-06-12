c1094bd8684d9d9cd53b5b4bbcb26b5da58759b9
ValichkaAW <51704325+ValichkaAW@users.noreply.github.com>
2019-06-12 09:02:24 +0300

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,12 @@
+import datetime
+def printTimeStamp(name):
+    print('Автор програми: ' + name)
+    print('Час компіляції: ' + str(datetime.datetime.now()))
+n = input("Введите 4-значное число: ")
+l = list(n)
+d1 = int(l[0])
+d2 = int(l[1])
+d3 = int(l[2])
+d4 = int(l[3])
+print("Сумма цифр числа:", d1 + d2 + d3+ d4 )
+printTimeStamp("Доброштан і Глигало")
---
+++ 9.py
@@ -0,0 +1,7 @@
+import datetime
+def printTimeStamp(name):
+    print('Автор програми: ' + name)
+    print('Час компіляції: ' + str(datetime.datetime.now()))
+l = int(input("Ваш ріст: "))
+print("Ріст в дюймах: ", l/2.54, "а ріст в футах: ", l/2.54/12)
+printTimeStamp("Доброштан і Глигало")
